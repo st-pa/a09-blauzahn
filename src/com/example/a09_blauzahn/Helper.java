9eaaf35954038af10af40aa483b452701e8095db
st.pa. <st_pa@gmx.net>
2014-11-26 10:25:46 +0100

    first commit

---
+++ src/com/example/a09_blauzahn/Helper.java
@@ -0,0 +1,64 @@
+package com.example.a09_blauzahn;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import android.bluetooth.BluetoothAdapter;
+
+public class Helper {
+
+	private static final Map<Integer,String> CONST_SCANMODE;
+	static {
+		CONST_SCANMODE = new TreeMap<Integer,String>();
+		CONST_SCANMODE.put(BluetoothAdapter.SCAN_MODE_NONE,"ScanModeNone");
+		CONST_SCANMODE.put(BluetoothAdapter.SCAN_MODE_CONNECTABLE,"ScanModeConnectable");
+		CONST_SCANMODE.put(BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE,"ScanModeConnectableDiscoverable");
+	}
+
+	/**
+	 * liefert eine mehrzeilige Text-Beschreibung des gegebenen objektes.
+	 * @param ba {@link BluetoothAdapter}
+	 * @return
+	 */
+	public static final String getDescription(BluetoothAdapter ba) {
+		StringBuffer s = new StringBuffer();
+		if (ba != null) {
+			s
+			.append("Adresse = \"")
+			.append(ba.getAddress())
+			.append("\"\n")
+
+			.append("Name = \"")
+			.append(ba.getName())
+			.append("\"\n")
+
+			.append("suchend : ")
+			.append(ba.isDiscovering() ? "ja" : "nein")
+			.append("\n")
+
+			.append("aktiviert : ")
+			.append(ba.isEnabled() ? "ja" : "nein")
+			.append("\n")
+
+			.append("scanMode = ")
+			.append(CONST_SCANMODE.get(ba.getScanMode()))
+			.append("\n")
+
+			.append("LEAdvertiser : ")
+			.append(ba.getBluetoothLeAdvertiser() == null ? "nicht " : " ")
+			.append("vorhanden\n")
+
+			.append("multi-ad-support : ")
+			.append(ba.isMultipleAdvertisementSupported() ? "ja" : "nein")
+			.append("\n")
+
+			.append("offload filtering : ")
+			.append(ba.isOffloadedFilteringSupported() ? "ja" : "nein")
+			.append("\n")
+			;
+		} else {
+			s.append("Kein Blauzahn-Anschluß vorhanden.\n");
+		}
+		return s.toString();
+	}
+}
---
+++ src/com/example/a09_blauzahn/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.a09_blauzahn;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity
+extends ActionBarActivity {
+
+	/** Kennzeichnung von Log-Meldungen. */
+	private static final String TAG = "Blauzahn";
+	/** ist der "sichtbare" name dieses blauzahns für andere geräte. */
+	private static final String BT_NAME = "Hallo Welt!";
+	/** soll als resultat der blauzahn-einschalte-aktivität zurückkommen. */
+	private static final int REQUEST_ENABLE_BT = 42;
+
+	private BluetoothAdapter ba;
+	private TextView tvLabel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		tvLabel = (TextView) findViewById(R.id.tvLabel);
+
+		StringBuffer s = new StringBuffer();
+		ba = BluetoothAdapter.getDefaultAdapter();
+		if (ba != null) {
+			s.append("Blauzahn gefunden.");
+//			ba.setName(BLAUZAHN_NAME);
+			
+			if (!ba.isEnabled()) {
+				Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				// @see #onActivityResult()
+				startActivityForResult(enableBluetoothIntent,REQUEST_ENABLE_BT);
+			}
+		} else {
+			s.append("Kein Blauzahn gefunden.");
+		}
+		tvLabel.setText(s.toString());
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			tvLabel.setText(Helper.getDescription(ba));
+			if (ba.isEnabled()) {
+				doDiscoverBT();
+			}
+		}
+	}
+
+	/** entdecke Blauzahn-Geräte. */
+	private void doDiscoverBT() {
+		final BroadcastReceiver br = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				String action = intent.getAction();
+				if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+					BluetoothDevice device = intent
+					.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+					Log.d(
+						TAG,
+						String.format(
+							"gefunden: %s [%s]",
+							device.getName(),
+							device.getAddress()
+						)
+					);
+					if (!ba.getBondedDevices().contains(device)) {
+						doPairDevice(device);
+					}
+					Toast.makeText(
+						getApplication(),
+						Helper.getDescription(ba),
+						Toast.LENGTH_SHORT
+					).show();
+				}
+			}
+		};
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(br,filter);
+	}
+
+	private void doPairDevice(BluetoothDevice device) {
+		try {
+			Method m = device.getClass().getMethod("createBond", (Class[]) null);
+			m.invoke(device,(Class[]) null);
+			Toast.makeText(
+				getApplication(),
+				String.format(
+					"paired with %s [%s]",
+					device.getName(),
+					device.getAddress()
+				),
+				Toast.LENGTH_SHORT
+			).show();
+			
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
